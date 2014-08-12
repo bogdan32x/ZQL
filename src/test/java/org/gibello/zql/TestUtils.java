2fb7d874dabad2559c0d0461464b4f557b57cca4
Bogdan Mariesan <bogdan.mariesan@gmail.com>
2014-08-12 15:54:49 +0300

    Added unit test and demo
    Usage demo. This class will be referred to on the GitHub README.md

---
+++ src/test/java/org/gibello/zql/TestUtils.java
@@ -0,0 +1,29 @@
+package org.gibello.zql;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Utils class to be used in unit testing the project.
+ * 
+ * @author Bogdan Mariesan, Romania
+ * @since 12.08.2014
+ *
+ */
+public class TestUtils {
+
+    public static String readInputStreamAsString(String inputStreamPath) throws IOException {
+        InputStream inputStream = new DataInputStream(new FileInputStream(inputStreamPath));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length = 0;
+        while ((length = inputStream.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
+        }
+        return baos.toString();
+    }
+
+}
--- src/test/java/org/gibello/zql/ZqlParserTest.java
+++ src/test/java/org/gibello/zql/ZqlParserTest.java
@@ -1,5 +1,14 @@
 package org.gibello.zql;
 
+import static org.junit.Assert.*;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -8,16 +17,55 @@ import org.mockito.runners.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class ZqlParserTest {
 
+    private static final String SRC_TEST_RESOURCES_VALID_SELECT_WITH_WILDCARD_SQL = "src/test/resources/valid_select_with_wildcard.sql";
     @Mock
     private ZqlParser zqlParser;
 
     @Test
-    public void test_simple_select_with_wildcard() {
+    public void test_simple_select_with_wildcard() throws IOException, ParseException {
         // given
-
-        // then
-
+        DataInputStream is = given_a_valid_select_with_a_wildcard_operator();
+        String rawSqlContent = TestUtils.readInputStreamAsString(SRC_TEST_RESOURCES_VALID_SELECT_WITH_WILDCARD_SQL);
         // when
+        ZqlParser parser = when_the_parser_is_initialized(is);
+        // then
+        then_given_sql_should_be_the_same_as_the_parsed_sql(parser, rawSqlContent);
+    }
+
+    private void then_given_sql_should_be_the_same_as_the_parsed_sql(ZqlParser parser, String rawSqlContent) throws ParseException, IOException {
+        assertNotNull(parser);
+        assertNotNull(rawSqlContent);
+
+        List<ZStatement> parsedExpressions = parser.readStatements();
+
+        assertNotNull(parsedExpressions);
+        assertTrue(parsedExpressions.size() > 0);
+
+        for (ZStatement statement : parsedExpressions) {
+            assertNotNull(statement);
+        }
+
+        assertNotNull(rawSqlContent);
+
+        for (ZStatement statement : parsedExpressions) {
+            if (statement instanceof ZQuery) {
+                ZQuery query = (ZQuery) statement;
+                rawSqlContent = rawSqlContent.replace(query.toString() + ";", "");
+            }
+        }
+
+        // if the parsed content and input match the result of the above function will render the raw string as an empty spaces string.
+        assertTrue(rawSqlContent.replaceAll("\\s", "").isEmpty());
+    }
+
+    private ZqlParser when_the_parser_is_initialized(DataInputStream is) {
+        ZqlParser parser = new ZqlParser(is);
+        return parser;
+    }
+
+    private DataInputStream given_a_valid_select_with_a_wildcard_operator() throws FileNotFoundException {
+        DataInputStream dis = new DataInputStream(new FileInputStream(new File(SRC_TEST_RESOURCES_VALID_SELECT_WITH_WILDCARD_SQL)));
+        return dis;
     }
 
 }
