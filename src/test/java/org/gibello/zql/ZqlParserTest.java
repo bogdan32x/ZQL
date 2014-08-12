f5ae4eb75d4b4bf84415c242e22dfb50987b9b0c
Bogdan Mariesan <bogdan.mariesan@gmail.com>
2014-08-12 12:35:14 +0300

    Added unit test sample
    This will be the template class for writing unit tests. It uses the BDD
    way of organizing methods and operations inside a method. Notice the
    give - when - then trio.

---
+++ src/test/java/org/gibello/zql/ZqlParserTest.java
@@ -0,0 +1,23 @@
+package org.gibello.zql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ZqlParserTest {
+
+    @Mock
+    private ZqlParser zqlParser;
+
+    @Test
+    public void test_simple_select_with_wildcard() {
+        // given
+
+        // then
+
+        // when
+    }
+
+}
