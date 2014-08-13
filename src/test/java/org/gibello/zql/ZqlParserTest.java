1248d3f3a107e4b8f7d651d88d2cf3c8f1727c93
Bogdan Mariesan <bogdan.mariesan@gmail.com>
2014-08-13 10:40:44 +0300

    Added test scenario
    Select from two tables with wildcard operator

--- src/test/java/org/gibello/zql/ZqlParserTest.java
+++ src/test/java/org/gibello/zql/ZqlParserTest.java
@@ -23,21 +23,47 @@ public class ZqlParserTest {
     @Mock
     private ZqlParser zqlParser;
 
-    private static final String TEST_RESOURCE_PATH_VALID_SELECT_WITH_WILDCARD = "src/test/resources/valid_select_with_wildcard.sql";
     private static final String VALID_SELECT_WITH_WILDCARD_EXPECTED_RESULT = "select * from Stock s";
+    private static final String VALID_SELECT_FROM_TWO_TABLES_WITH_WILDCARD_EXPECTED_RESULT = "select * from Stock s, Market m";
 
     @Test
     public void test_simple_select_with_wildcard() throws IOException, ParseException {
         // given
         DataInputStream is = given_a_valid_select_with_a_wildcard_operator();
-        String rawSqlContent = TestUtils.readInputStreamAsString(TEST_RESOURCE_PATH_VALID_SELECT_WITH_WILDCARD);
+        String rawSqlContent = TestUtils.readInputStreamAsString("src/test/resources/valid_select_with_wildcard.sql");
         // when
         ZqlParser parser = when_the_parser_is_initialized(is);
         // then
-        then_given_sql_should_be_the_same_as_the_parsed_sql(parser, rawSqlContent);
+        then_given_sql_should_be_the_same_as_the_parsed_sql(parser, rawSqlContent, VALID_SELECT_WITH_WILDCARD_EXPECTED_RESULT);
     }
 
-    private void then_given_sql_should_be_the_same_as_the_parsed_sql(ZqlParser parser, String rawSqlContent) throws ParseException, IOException {
+    @Test
+    public void test_select_from_two_tables_with_wildcard() throws IOException, ParseException {
+        // given
+        DataInputStream is = given_a_valid_select_from_two_tables_with_a_wildcard_operator();
+        String rawSqlContent = TestUtils.readInputStreamAsString("src/test/resources/valid_select_from_two_tables_with_wildcard.sql");
+        // when
+        ZqlParser parser = when_the_parser_is_initialized(is);
+        // then
+        then_given_sql_should_be_the_same_as_the_parsed_sql(parser, rawSqlContent, VALID_SELECT_FROM_TWO_TABLES_WITH_WILDCARD_EXPECTED_RESULT);
+    }
+
+    private DataInputStream given_a_valid_select_from_two_tables_with_a_wildcard_operator() throws FileNotFoundException {
+        DataInputStream dis = new DataInputStream(new FileInputStream(new File("src/test/resources/valid_select_from_two_tables_with_wildcard.sql")));
+        return dis;
+    }
+
+    private ZqlParser when_the_parser_is_initialized(DataInputStream is) {
+        ZqlParser parser = new ZqlParser(is);
+        return parser;
+    }
+
+    private DataInputStream given_a_valid_select_with_a_wildcard_operator() throws FileNotFoundException {
+        DataInputStream dis = new DataInputStream(new FileInputStream(new File("src/test/resources/valid_select_with_wildcard.sql")));
+        return dis;
+    }
+
+    private void then_given_sql_should_be_the_same_as_the_parsed_sql(ZqlParser parser, String rawSqlContent, String expectedResult) throws ParseException, IOException {
         assertNotNull(parser);
         assertNotNull(rawSqlContent);
 
@@ -55,19 +81,8 @@ public class ZqlParserTest {
         for (ZStatement statement : parsedExpressions) {
             if (statement instanceof ZQuery) {
                 ZQuery query = (ZQuery) statement;
-                assertEquals(VALID_SELECT_WITH_WILDCARD_EXPECTED_RESULT, query.toString());
+                assertEquals(expectedResult, query.toString());
             }
         }
     }
-
-    private ZqlParser when_the_parser_is_initialized(DataInputStream is) {
-        ZqlParser parser = new ZqlParser(is);
-        return parser;
-    }
-
-    private DataInputStream given_a_valid_select_with_a_wildcard_operator() throws FileNotFoundException {
-        DataInputStream dis = new DataInputStream(new FileInputStream(new File(TEST_RESOURCE_PATH_VALID_SELECT_WITH_WILDCARD)));
-        return dis;
-    }
-
 }
